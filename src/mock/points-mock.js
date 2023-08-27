c7642957e585853282f1ec585ea8efd133c51d58
Divashka <dianakuzmina325@gmail.com>
2023-08-27 15:53:29 +0300

    Удаление комментариев

--- src/mock/points-mock.js
+++ src/mock/points-mock.js
@@ -1,28 +1,9 @@
 import { getRandomArrayElement, getRandomInteger } from '../utils';
 import { createRandomDates } from '../mock/dates';
 import { POINT_TYPES } from '../const';
-//import { createListOffers } from './offers-mock';
-// import { createDestination } from './destinations-mock';
 
 const MIN_PRICE = 100;
 const MAX_PRICE = 10000;
-
-//const offers = Array.from({ length: OFFERS_COUNT }, createListOffers);
-// const destinations = Array.from({ length: getRandomInteger(1, DESTINATIONS_COUNT) }, (_, index) => createDestination(index));
-
-// function getRandomIds() {
-//   const randomOffers = getRandomArrayElement(offers).offers;
-
-//   const ids = [];
-//   const length = getRandomInteger(1, randomOffers.length);
-//   while (ids.length < length) {
-//     const currentElement = getRandomInteger(1, randomOffers.length);
-//     if (!ids.includes(currentElement)) {
-//       ids.push(currentElement);
-//     }
-//   }
-//   return ids;
-// }
 
 function createPoint(index, ids, destinationId) {
   const randomDates = createRandomDates();
