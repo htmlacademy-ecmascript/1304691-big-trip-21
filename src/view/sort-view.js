33adbb8035b67fb12be4c04728a5048064df0291
Divashka <dianakuzmina325@gmail.com>
2023-09-05 19:15:33 +0300

    Сортировка сортирует

--- src/view/sort-view.js
+++ src/view/sort-view.js
@@ -4,8 +4,17 @@ import { capitalizeFirstLetter } from '../utils/common.js';
 
 function createSortItem() {
   return Object.values(SortType).map((value) => `<div class="trip-sort__item  trip-sort__item--${value}">
-    <input id="sort-${value}" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-${value}" checked>
-        <label class="trip-sort__btn" for="sort-${value}">${capitalizeFirstLetter(value)}</label>
+    <input
+    id="sort-${value}"
+    class="trip-sort__input
+    visually-hidden"
+    type="radio"
+    name="trip-sort"
+    value="sort-${value}"
+    ${value === SortType.EVENT || value === SortType.OFFER ? 'disabled' : ''}
+    ${value === SortType.DEFAULT ? 'checked' : ''}
+    >
+      <label class="trip-sort__btn" for="sort-${value}" data-sort-type="${value}">${capitalizeFirstLetter(value)}</label>
     </div>
   `).join('');
 }
@@ -29,12 +38,26 @@ export default class SortView extends AbstractView {
   }
 
   #onSortTypeChange = (evt) => {
-    if (evt.target !== 'a') {
+    if (evt.target.tagName !== 'LABEL') {
       return;
     }
     evt.preventDefault();
-    this.#onSortChange();
+
+    const currentTypeElement = evt.target;
+    const currentTypeItemElement = currentTypeElement.closest('.trip-sort__item');
+    const currentInput = currentTypeItemElement.querySelector('.trip-sort__input');
+
+    this.#onSortChange(currentTypeElement.dataset.sortType);
+    this.#setAttributeSort(currentInput);
   };
+
+  #setAttributeSort(currentInput) {
+    currentInput.removeAttribute('checked');
+    if (currentInput.hasAttribute('checked') || currentInput.hasAttribute('disabled')) {
+      return;
+    }
+    currentInput.setAttribute('checked', 'checked');
+  }
 
   get template() {
     return createSortTemplate();
