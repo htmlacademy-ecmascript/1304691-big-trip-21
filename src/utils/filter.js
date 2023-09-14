b0b4158745521990dfde57562d68d4b80ee6ec64
Divashka <dianakuzmina325@gmail.com>
2023-09-14 12:33:02 +0300

    Исправление фильтра

--- src/utils/filter.js
+++ src/utils/filter.js
@@ -1,20 +1,12 @@
 import dayjs from 'dayjs';
 import { FilterType } from '../const';
-import isSameOrBefore from 'dayjs/plugin/isSameOrBefore';
-import isSameOrAfter from 'dayjs/plugin/isSameOrAfter';
-import utc from 'dayjs/plugin/utc';
 
-dayjs.extend(utc);
-dayjs.extend(isSameOrBefore);
-dayjs.extend(isSameOrAfter);
-
-const currentDate = dayjs.utc().format();
-
-const filter = {
+const Filters = {
   [FilterType.ALL]: (points) => points,
-  [FilterType.FUTURE]: (points) => points.filter((point) => dayjs(point.dateFrom).isSameOrAfter(currentDate)),
-  [FilterType.PRESENT]: (points) => points.filter((point) => dayjs(point.dateFrom).isSameOrBefore(currentDate, 'day') && dayjs(point.dateTo).isSameOrAfter(currentDate, 'day')),
-  [FilterType.PAST]: (points) => points.filter((point) => dayjs(point.dateTo).isSameOrBefore(currentDate)),
+  [FilterType.FUTURE]: (points) => points.filter((point) => dayjs().isBefore(dayjs(point.dateFrom))),
+  [FilterType.PRESENT]: (points) => points.filter((point) => dayjs().isAfter(dayjs(point.dateFrom)) && dayjs().isBefore(dayjs(point.dateTo))),
+  [FilterType.PAST]: (points) => points.filter((point) => dayjs().isAfter(dayjs(point.dateTo))),
 };
 
-export { filter };
+
+export { Filters };
