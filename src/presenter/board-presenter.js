8693aaf1cd3af9b038610b827b030e0058cedef5
Divashka <dianakuzmina325@gmail.com>
2023-09-14 12:38:33 +0300

    points-presenter на board-presenter

---
+++ src/presenter/board-presenter.js
@@ -0,0 +1,142 @@
+import { RenderPosition, render } from '../framework/render';
+import SortView from '../view/sort-view';
+import ListView from '../view/list-view';
+import NoPointsView from '../view/no-points-view';
+import PointPresenter from './point-presenter';
+import { updateItem } from '../utils/common';
+import { SortType, enabledSortType } from '../const';
+import { sortPointsByTime, sortPointsByPrice } from '../utils/common';
+
+const tripEvents = document.querySelector('.trip-events');
+
+export default class PointsPresenter {
+  #pointsModel = null;
+  #offersModel = null;
+  #destinationsModel = null;
+  #pointsListComponent = new ListView();
+  #sortComponent = null;
+  #noPointsComponent = new NoPointsView();
+  #currentSortType = SortType.DEFAULT;
+  #sourcedPoints = [];
+
+  #points = [];
+  #pointPresenters = new Map();
+
+  constructor({pointsModel, offersModel, destinationsModel}) {
+    this.#pointsModel = pointsModel;
+    this.#offersModel = offersModel;
+    this.#destinationsModel = destinationsModel;
+
+    this.#points = [...this.#pointsModel.points];
+    this.#sourcedPoints = [...this.#pointsModel.points];
+  }
+
+  init() {
+    this.#renderBoard();
+  }
+
+  #onPointChange = (updatedPoint) => {
+    this.#points = updateItem(this.#points, updatedPoint);
+    this.#sourcedPoints = updateItem(this.#sourcedPoints, updatedPoint);
+    this.#pointPresenters.get(updatedPoint.id).init(updatedPoint);
+  };
+
+  #onModeChange = () => {
+    this.#pointPresenters.forEach((presenter) => presenter.resetView());
+  };
+
+  #onSortChange = (sortType) => {
+    if (this.#currentSortType === sortType) {
+      return;
+    }
+
+    this.#sortPoints(sortType);
+    this.#clearPointsList();
+    this.#renderPoints();
+  };
+
+  #sortPoints(sortType) {
+
+    switch (sortType) {
+      case SortType.TIME:
+        this.#points.sort(sortPointsByTime);
+        break;
+      case SortType.PRICE:
+        this.#points.sort(sortPointsByPrice);
+        break;
+      case SortType.DEFAULT:
+        this.#points = [...this.#sourcedPoints];
+        break;
+    }
+
+    this.#currentSortType = sortType;
+
+  }
+
+  #renderSort() {
+    const sortItems = this.#generateSort();
+
+    this.#sortComponent = new SortView({
+      sortItems: sortItems,
+      onSortChange: this.#onSortChange
+    });
+
+    render(this.#sortComponent, this.#pointsListComponent.element, RenderPosition.AFTERBEGIN);
+  }
+
+  #generateSort() {
+    return Object.values(SortType).map(
+      (type) => ({
+        type,
+        isChecked: (type === this.#currentSortType),
+        isDisabled: !enabledSortType[type]
+      }),
+    );
+  }
+
+  #renderPoints() {
+    this.#points.forEach((point) => {
+      this.#renderPoint(point);
+    });
+  }
+
+  #renderPoint(point) {
+
+    const pointPresenter = new PointPresenter(
+      {
+        containerPoints: this.#pointsListComponent.element,
+        offersModel: this.#offersModel,
+        destinationsModel: this.#destinationsModel,
+        pointsModel: this.#pointsModel,
+        onDataChange: this.#onPointChange,
+        onModeChange: this.#onModeChange,
+      }
+    );
+
+    pointPresenter.init(point);
+    this.#pointPresenters.set(point.id, pointPresenter);
+  }
+
+  #clearPointsList() {
+    this.#pointPresenters.forEach((presenter) => presenter.destroy());
+    this.#pointPresenters.clear();
+  }
+
+  #renderBoard() {
+    if (this.#points.length === 0) {
+      this.#renderNoPoints();
+      return;
+    }
+    
+    render(this.#pointsListComponent, tripEvents);
+
+    this.#renderSort();
+    this.#renderPoints();
+  }
+
+  #renderNoPoints() {
+    render(this.#noPointsComponent, this.#pointsListComponent.element);
+  }
+
+}
+
