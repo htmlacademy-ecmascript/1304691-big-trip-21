5e2c88938e6c78b79ae8034e844578e40f9b11c8
Divashka <dianakuzmina325@gmail.com>
2023-09-28 15:51:36 +0300

    7.7 Добавит модель для фильтра

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import HeaderPresenter from './presenter/header-presenter';
 import PointsModel from './model/points-model';
 import OffersModel from './model/offers-model';
 import DestinationsModel from './model/destinations-model';
+import FilterModel from './model/filter-model.js';
 
 import Service from './service/service';
 
@@ -12,6 +13,7 @@ const service = new Service();
 const pointsModel = new PointsModel(service);
 const offersModel = new OffersModel(service);
 const destinationsModel = new DestinationsModel(service);
+const filterModel = new FilterModel();
 
 const pointsPresenter = new PointsPresenter({pointsModel, offersModel, destinationsModel});
 
